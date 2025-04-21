c28093f01fef8c4f90ab506f464449a134f1fb65
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2025-04-21 18:55:01 -0400

    2025.4.0

--- generator/src/test/java/org/nasdanika/models/family/generator/tests/TestFamilyModelDocGen.java
+++ generator/src/test/java/org/nasdanika/models/family/generator/tests/TestFamilyModelDocGen.java
@@ -105,7 +105,7 @@ public class TestFamilyModelDocGen {
 		
 		System.out.println("There are " + errorCount + " site errors");
 		
-		if (errorCount != 65) {
+		if (errorCount != 52) {
 			throw new ExecutionException("There are problems with pages: " + errorCount);
 		}		
 	}
