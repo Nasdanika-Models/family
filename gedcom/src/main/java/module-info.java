3e632c2498196248dd882b22c6ebffeaf1b0d540
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2026-09-01 09:11:12 -0400

    Initial gedcom loader module

---
+++ gedcom/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.nasdanika.models.family.gedcom {
+	
+//	exports org.nasdanika.models.family.gedcom;
+//	opens org.nasdanika.models.family.gedcom to org.nasdanika.common; // For transformer
+		
+	requires transitive org.nasdanika.models.family;
+	
+}
